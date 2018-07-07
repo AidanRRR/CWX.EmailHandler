41188003e33305e451a8f923afc4e4fefee0c8c6
LAPTOP-3D6CBM0D\Aidan <aidan.rypens@outlook.com>
2018-07-07 16:59:04 +0200

    Added email handler

---
+++ EmailHandler/Handler.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace EmailHandler
+{
+    public static class Handler
+    {
+        [FunctionName("EmailHandler")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, null, Route = "{*url}")]HttpRequestMessage req, TraceWriter log, ExecutionContext context)
+        {
+            if (req.RequestUri.Segments.Length != 5)
+            {
+                return new HttpResponseMessage();
+            }
+
+            var email = $"{req.RequestUri.Segments[2].Replace("/", "")}" +
+                                   $"@{req.RequestUri.Segments[3].Replace("/", "")}" +
+                                   $".{req.RequestUri.Segments[4].Replace("/", "")}";
+
+            var formData = req.GetQueryNameValuePairs();
+            const string margin = "<span style='padding-bottom: 5px;'";
+            string formDataFormatted = String.Join($"{margin}<br />", formData.Select(kv => kv.Key + ": " + "<b>" + kv.Value + "</b>"));
+
+            const string apiKey = "";
+            var client = new SendGridClient(apiKey);
+
+            var from = new EmailAddress(email);
+            const string subject = "New form submission";
+            var to = new EmailAddress(email);
+
+            const string plainTextContent = "";
+            var htmlContent = "<h1>New form submission</h1>" + $"{formDataFormatted} <br /> <br /> <br />";
+
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
